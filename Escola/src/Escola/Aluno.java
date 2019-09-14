82ac22f8342b2b8a986d41fc69ec4c14b68712cc
tallys augusto <tallysmugetsu@gmail.com>
2019-09-13 21:20:00 -0300

    iniciando projeto de java

---
+++ Escola/src/Escola/Aluno.java
@@ -0,0 +1,8 @@
+package Escola;
+
+public class Aluno {
+	String nome;
+	int idade;
+	double nota1;
+	double nota2;
+}
---
+++ Escola/src/Escola/CriandoAluno.java
@@ -0,0 +1,14 @@
+package Escola;
+
+public class CriandoAluno {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		a1.nome = "Tallys";
+		a1.idade = 26;
+		a1.nota1 = 9.5;
+		a1.nota2 = 8.9;
+
+	}
+
+}
